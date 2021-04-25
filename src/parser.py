fa5557b56c04b5271f6a08e69686995fad172498
abiwardani <abiwardani85@gmail.com>
2021-04-25 15:26:09 +0700

    draft UI + draft parser

---
+++ src/parser.py
@@ -0,0 +1,111 @@
+import re
+
+formatTanggal1 = '([0-3]?[0-9]) ([a-zA-Z]+) ([0-9]{4})' #tanggal bulan tahun
+formatTanggal2 = '([a-zA-Z]+) ([0-3]?[0-9]) ([0-9]{4})' #bulan tanggal tahun
+formatTanggal3 = '([0-3][0-9])/([0-9][0-9])/([0-9]{4})' #DD/MM/YYYY
+formatJenis = '([Tt]ubes|[Tt]ucil|[Kk]uis|[Uu]jian|[Pp]raktikum)'
+
+def objek(text):
+    try:
+        o = re.search(formatJenis+' (.+?) pada', text).group(2)
+    except AttributeError:
+        try:
+            o = re.search(formatJenis+' (.+?)$', text).group(2)
+        except AttributeError:
+            o = None
+        
+    return o
+
+def matkul(text):
+    try:
+        s = re.search('([A-Z]{2}[0-9]{4}) ?(.*)$', text).group(1)
+    except AttributeError:
+        s = None
+        
+    return s
+
+def topik(text):
+    try:
+        t = re.search('([A-Z]{2}[0-9]{4}) ?(.+)$', text).group(2)
+    except AttributeError:
+        t = None
+        
+    return t
+    
+def jenis(text):
+    try:
+        j = re.search(formatJenis, text).group(0)
+    except AttributeError:
+        try:
+            j = re.search('[Dd]eadline', text).group(0)
+        except AttributeError:
+            j = None
+        
+    return j
+
+def tanggalPada(text):
+    try:
+        tp = re.search('pada ([a-zA-Z]+), (.+?)$', text).group(2)
+    except AttributeError:
+        try:
+            tp = re.search('pada (.+?),', text).group(1)
+        except AttributeError:
+            try:
+                tp = re.search('pada (.+?)$', text).group(1)
+            except AttributeError:
+                tp = None
+        
+    return tp
+    
+def tanggalTipe(text):
+    try:
+        tp = re.search(formatTanggal1, text).group(0)
+        type = 1
+    except AttributeError:
+        try:
+            tp = re.search(formatTanggal2, text).group(0)
+            type = 2
+        except AttributeError:
+            try:
+                tp = re.search(formatTanggal3, text).group(0)
+                type = 3
+            except AttributeError:
+                type = None
+    
+def pertanyaan(text):
+    try:
+        tp = re.search('?$', text).group(0)
+    except:
+        tp = None
+    
+    return (tp != None)
+    
+def task(text):
+    try:
+        tp = int(re.search('[Tt]ask ([0-9]+)', text).group(1))
+    except AttributeError:
+        tp = None
+        
+    return None
+    
+def translateBulan(bulan):
+    listbulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+    listmonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    
+    try:
+        id = listbulan.index(bulan)
+        month = listmonth[id]
+    except:
+        month = None
+    
+    return month
+    
+#test1 = "tolong ingetin ada Tubes IF2211 String Matching pada 11 April 2021"
+#test2 = "Kuis IF2230 pada 14/04/2021"
+#test3 = "Tolong ingatkan ada Ujian IF2240 Bab 10 pada Senin, 12 Mei 2021"
+#test4 = "Apa saja deadline yang dimiliki sejauh ini?"
+#test5 = "3 minggu ke depan ada kuis apa saja?"
+#test6 = "Ada kuis apa saja untuk 3 minggu ke depan?"
+#test7 = "Apa saja deadline antara 03/04/2021 sampai 15/04/2021?"
+#test8 = "Tubes IF2211 String Matching pada 14 April 2021"
+#test9 = "Halo bot, tolong ingetin kalau ada Tucil IF2220 Bab 2 pada 22/04/21"
---
+++ src/schedline.py
@@ -0,0 +1,195 @@
+import os
+import datetime
+import parser as p
+from flask import Flask, render_template, flash, request, url_for, redirect
+from werkzeug.utils import secure_filename
+
+DATA_FOLDER = '/data'
+ALLOWED_EXTENSIONS = {'txt','pdf'}
+app = Flask(__name__)
+app.config['DATA_FOLDER'] = DATA_FOLDER
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/')#, methods = ['GET', 'POST'])
+def mainPage():
+    #if request.method == "POST":
+    #    n = request.form.get("message")
+    
+    return render_template('mainpage.html')
+    
+def processInput(text):
+    o = p.objek(text)
+    m = p.matkul(o)
+    t = p.topik(o)
+    j = p.jenis(text)
+    if (p.pertanyaan(text)):
+        print("pertanyaan")
+    else:
+        tp = p.tanggalPada(text)
+        response = "[TASK BERHASIL DICATAT]"
+        response += "<br>"+j.capitalize()+" pada "+tp+" untuk "+m+": "+t.capitalize()+" berhasil dicatat"
+        
+        now = datetime.datetime.now()
+        f = open("./data/logs.txt", "a")
+        log = "B"+now.strftime("%m/%d/%Y %H:%M:%S")+response+"\n"
+        f.write(log)
+        f.close()
+    
+@app.route('/Chat', methods = ['GET', 'POST'])
+def chatPage():
+    f = open("./data/logs.txt", "a")
+    
+    if request.method == "POST":
+        msg = request.form.get("messageInput").lstrip()
+        if (msg.count(" ") < len(msg)):
+            now = datetime.datetime.now()
+            log = "U"+now.strftime("%m/%d/%Y %H:%M:%S")+msg+"\n"
+            f.write(log)
+            f.close()
+            processInput(msg)
+    
+    f = open("./data/logs.txt", "r")
+    chatLogs = f.readlines()
+    
+    html = ''
+    html += '<!DOCTYPE html>\n'
+    html += '<html>\n'
+    html += '    <head>\n'
+    html += '        <title>SchedLINE</title>\n'
+    html += '        <style>\n'
+    html += '            div.chatscroll {\n'
+    html += '                overflow-y: auto;\n'
+    html += '                border: 1px solid black;\n'
+    html += '                height: 500px;\n'
+    html += '                width: 400px;\n'
+    html += '                display: flex;\n'
+    html += '                flex-direction: column-reverse;\n'
+    html += '            }\n'
+    html += '            table.chat {\n'
+    html += '                width: 400px;\n'
+    html += '                border-collapse: separate;\n'
+    html += '                border-spacing: 10px 0px;\n'
+    html += '            }\n'
+    html += '            p.datebubble {\n'
+    html += '                border-radius: 5px 5px 5px 5px;\n'
+    html += '                background: #d9d9d9;\n'
+    html += '                padding: 3px;\n'
+    html += '                text-align: center;\n'
+    html += '                width: fit-content;\n'
+    html += '                float: center;\n'
+    html += '                font-weight: normal;\n'
+    html += '                font-family: Arial;\n'
+    html += '                font-size: 11px;\n'
+    html += '            }\n'
+    html += '            p.botbubble {\n'
+    html += '                border-radius: 5px 20px 20px 20px;\n'
+    html += '                background: #e3e3e3;\n'
+    html += '                padding: 10px;\n'
+    html += '                text-align: left;\n'
+    html += '                width: fit-content;\n'
+    html += '                max-width: 320px;\n'
+    html += '                float: left;\n'
+    html += '                font-weight: normal;\n'
+    html += '                font-family: Arial;\n'
+    html += '                font-size: 14px;\n'
+    html += '                word-wrap: break-word;\n'
+    html += '            }\n'
+    html += '            p.bottime {\n'
+    html += '                text-align: left;\n'
+    html += '                vertical-align: text-bottom;\n'
+    html += '                float: left;\n'
+    html += '                width: fit-content;\n'
+    html += '                font-weight: normal;\n'
+    html += '                font-family: Arial;\n'
+    html += '                font-size: 9px;\n'
+    html += '                color: #3c3c3c;\n'
+    html += '                padding: 5px;\n'
+    html += '            }\n'
+    html += '            p.userbubble {\n'
+    html += '                border-radius: 20px 5px 20px 20px;\n'
+    html += '                background: #d0e3bc;\n'
+    html += '                padding: 10px;\n'
+    html += '                text-align: right;\n'
+    html += '                width: fit-content;\n'
+    html += '                max-width: 320px;\n'
+    html += '                float: right;\n'
+    html += '                font-weight: normal;\n'
+    html += '                font-family: Arial;\n'
+    html += '                font-size: 14px;\n'
+    html += '                word-wrap: break-word;\n'
+    html += '            }\n'
+    html += '            p.usertime {\n'
+    html += '                text-align: right;\n'
+    html += '                vertical-align: bottom;\n'
+    html += '                float: right;\n'
+    html += '                width: fit-content;\n'
+    html += '                font-weight: normal;\n'
+    html += '                font-family: Arial;\n'
+    html += '                font-size: 9px;\n'
+    html += '                color: #3c3c3c;\n'
+    html += '                padding: 5px;\n'
+    html += '            }\n'
+    html += '            div.messageBox {\n'
+    html += '                width: 400px;\n'
+    html += '                border: 1px solid black;\n'
+    html += '            }\n'
+    html += '            \n'
+    html += '            #message {\n'
+    html += '                height: 24px;\n'
+    html += '                width: 340px;\n'
+    html += '            }\n'
+    html += '            \n'
+    html += '            #send {\n'
+    html += '                float: right;\n'
+    html += '                padding: 5px;\n'
+    html += '            }\n'
+    html += '            \n'
+    html += '        </style>\n'
+    html += '    </head>\n'
+    html += '    <body>\n'
+    html += '    <a style=\"text-decoration: none\" href=\"http://127.0.0.1:5000/Chat\"><h2>SchedLINE<span class=\"Searchy\"> Bot</span></h2></a>\n'
+    html += '    <div class=\"chatscroll\">\n'
+    html += '        <table class=\"chat\">\n'
+    
+    bar = None
+    
+    for line in chatLogs:
+        type = line[0]
+        date_time_obj = datetime.datetime.strptime(line[1:20], "%m/%d/%Y %H:%M:%S")
+        if ((bar == None) or (date_time_obj.date() > bar.date())):
+            bar = date_time_obj
+            datestr = date_time_obj.strftime("%m/%d/%Y")
+            now = datetime.datetime.now()
+            sDiff = (now-bar).total_seconds()
+            if sDiff < 60*60*24:
+                print(sDiff)
+                if bar.day == now.day:
+                    datestr = "Today"
+                else:
+                    datestr = "Yesterday"
+            html += '            <tr><td><p class=\"datebubble\">'+datestr+'</p></td></tr>\n'
+        message = line[20:-1]
+        if type == "U":
+            html += '            <tr><td><p class=\"userbubble\">'+message+'</p>'
+            html += '<p class=\"usertime\">'+date_time_obj.time().isoformat(timespec='minutes')+'</p></td></tr>\n'
+        elif type == "B":
+            html += '            <tr><td><p class=\"botbubble\">'+message+'</p>'
+            html += '<p class=\"bottime\">'+date_time_obj.time().isoformat(timespec='minutes')+'</p></td></tr>\n'
+    
+    html += '        </table>\n'
+    html += '    </div>\n'
+    html += '    <form action=\"http://127.0.0.1:5000/Chat\" method=POST>\n'
+    html += '        <div class=\"messageBox\">\n'
+    html += '            <input type=\"text\" name=\"messageInput\" id=\"message\" placeholder=\"Type your message...\" autocomplete="off">\n'
+    html += '            <input type=\"submit\" id=\"send\" name=\"sendButton\" value=\"send\">\n'
+    html += '        </div>\n'
+    html += '        </form>\n'
+    html += '    </body>\n'
+    html += '</html>'
+    with open("templates/chat.html", "w", encoding="utf8") as file:
+        file.write(html)
+    
+    return html #render_template('chat.html')
+
+if __name__ == '__main__':
+    app.run()

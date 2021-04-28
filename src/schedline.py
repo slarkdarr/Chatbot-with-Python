48c6fab3cb53e7c6feb2fa930e3987f3aa3528e0
raffifpr <68497910+raffifpr@users.noreply.github.com>
2021-04-28 11:20:58 +0700

    Update schedline.py

--- src/schedline.py
+++ src/schedline.py
@@ -284,6 +284,29 @@ def levenshteinDistanceDP(A, B):
     #printDistances(distances, len(token1), len(token2))
     return distances[len(A)][len(B)]
 
+def calcDictDistance(word, numWords):
+    file = open('kamus.txt', 'r') 
+    lines = file.readlines() 
+    file.close()
+    dictWordDist = []
+    wordIdx = 0
+    
+    for line in lines: 
+        wordDistance = levenshteinDistanceDP(word, line.strip())
+        if wordDistance >= 10:
+            wordDistance = 9
+        dictWordDist.append(str(int(wordDistance)) + "-" + line.strip())
+        wordIdx = wordIdx + 1
+
+    closestWords = []
+    wordDetails = []
+    currWordDist = 0
+    dictWordDist.sort()
+    for i in range(numWords):
+        currWordDist = dictWordDist[i]
+        wordDetails = currWordDist.split("-")
+        closestWords.append(wordDetails[1])
+    return closestWords
 
   
 @app.route('/Chat', methods = ['GET', 'POST'])
